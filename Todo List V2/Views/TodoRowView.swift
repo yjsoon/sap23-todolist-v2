f9322bf5abc8f03b498ee07d4798d02416ead7cf
YJ Soon <yjsoon@tinkertanker.com>
2023-08-05 11:41:52 +0800

    Did some refactoring, started TodoRowView (but empty)

---
+++ Todo List V2/Views/TodoRowView.swift
@@ -0,0 +1,20 @@
+//
+//  TodoRowView.swift
+//  Todo List V2
+//
+//  Created by YJ Soon on 5/8/23.
+//
+
+import SwiftUI
+
+struct TodoRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodoRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoRowView()
+    }
+}
