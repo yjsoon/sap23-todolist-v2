bf24a630a0f8c031dfafc780549056a6a6358945
YJ Soon <yjsoon@tinkertanker.com>
2023-08-05 09:21:18 +0800

    Initial Commit

---
+++ Todo List V2/Todo_List_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_List_V2App.swift
+//  Todo List V2
+//
+//  Created by YJ Soon on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_List_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
